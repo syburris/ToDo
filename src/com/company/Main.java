a02f6df27bd1fa0be5248a0fdc612c61739f703c
Steven Burris <stevenburris.jr@gmail.com>
2016-09-27 09:42:27 -0400

    refactored methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -36,28 +36,13 @@ public class Main {
 
                 switch (option) {
                     case "1":
-                        System.out.println("Enter your to-do item:");
-                        String text = scanner.nextLine();
-                        Item item = new Item(text, false);
-                        items.add(item);
+                        addTodo(scanner,items);
                         break;
                     case "2":
-                        System.out.println("Which item would you like to toggle?");
-                        int i = Integer.valueOf(scanner.nextLine());
-                        Item item2 = items.get(i - 1);
-                        item2.isDone = !item2.isDone;
+                        toggleTodo(scanner,items);
                         break;
                     case "3":
-                        for (int j = 0; j < items.size(); j++) {
-                            Item item3 = items.get(j);
-                            int num = j + 1;
-                            String checkbox = "[ ]";
-                            if (item3.isDone) {
-                                checkbox = "[X]";
-                            }
-                            System.out.println(checkbox + " " + num + ". " + item3.text);
-                            System.out.printf("%s %s. %s\n", checkbox, num, item3.text); //does the same thing as the previous line
-                        }
+                        toDoList(items);
                         break;
                     case "4":
                         isLoggedIn = false;
@@ -68,6 +53,30 @@ public class Main {
                         break;
                 }
             }
+        }
+    }
+    public static void addTodo(Scanner scanner, ArrayList<Item> items) {
+        System.out.println("Enter your to-do item:");
+        String text = scanner.nextLine();
+        Item item = new Item(text, false);
+        items.add(item);
+    }
+    public static void toggleTodo(Scanner scanner, ArrayList<Item> items) {
+        System.out.println("Which item would you like to toggle?");
+        int i = Integer.valueOf(scanner.nextLine());
+        Item item2 = items.get(i - 1);
+        item2.isDone = !item2.isDone;
+    }
+    public static void toDoList (ArrayList<Item> items) {
+        for (int j = 0; j < items.size(); j++) {
+            Item item3 = items.get(j);
+            int num = j + 1;
+            String checkbox = "[ ]";
+            if (item3.isDone) {
+                checkbox = "[X]";
+            }
+            System.out.println(checkbox + " " + num + ". " + item3.text);
+            System.out.printf("%s %s. %s\n", checkbox, num, item3.text); //does the same thing as the previous line
         }
     }
 }
